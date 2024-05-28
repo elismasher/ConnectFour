65fd44b24360d9743a103f647e6a0284f008fa0e
Elismasher <rist24082001@gmail.com>
2024-05-28 17:35:39 +0200

    add implementation

--- src/main/java/spw4/connectfour/ConnectFourImpl.java
+++ src/main/java/spw4/connectfour/ConnectFourImpl.java
@@ -1,25 +1,51 @@
 package spw4.connectfour;
 
 public class ConnectFourImpl implements ConnectFour {
+    private Player currentPlayer;
+    private GameBoard gameBoard;
+
     public ConnectFourImpl(Player playerOnTurn) {
+        this.currentPlayer = playerOnTurn;
+        this.gameBoard = new GameBoard();
     }
 
     public Player getPlayerAt(int row, int col) {
-        return Player.none;
+        return gameBoard.getPlayerAt(row, col);
     }
+
     public Player getPlayerOnTurn() {
-        return Player.yellow;
+        return currentPlayer;
     }
+
     public boolean isGameOver() {
-        return false;
+        return gameBoard.checkForConnectFour();
     }
+
     public Player getWinner() {
+        if (isGameOver())
+            return currentPlayer == Player.red ? Player.yellow : Player.red;
         return Player.none;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nPlayer: ").append(currentPlayer == Player.red ? "RED" : "YELLOW").append("\n");
+        sb.append(gameBoard);
+        return sb.toString();
     }
 
     public void reset(Player playerOnTurn) {
+        currentPlayer = playerOnTurn;
+        gameBoard = new GameBoard();
     }
 
     public void drop(int col) {
+        if (gameBoard.drop(col, currentPlayer))
+            switchCurrentPlayer();
+    }
+
+    private void switchCurrentPlayer() {
+        currentPlayer = currentPlayer == Player.red ? Player.yellow : Player.red;
     }
 }
